0183671b25b859fdd2989beb685414e4a085231c
Doomfangre <Doomfangre@gmail.com>
2026-04-30 23:58:49 -0700

    ...

--- action.js
+++ action.js
@@ -3,8 +3,9 @@ function goneagainO() {
     document.getElementById("option2").style.visibility = "hidden";
 }
 
+let health = 9;
+
 function quick1() {
-    let key = 0;
     document.getElementById("qte1").style.transform = `translate(0px, 0px)`;
     document.getElementById("qte1").style.backgroundImage = `url(qte1.gif)`;
     const randomer = Math.floor(Math.random() * 700) + 100;
@@ -15,20 +16,16 @@ function quick1() {
     if (event.key === 'w') {
         document.getElementById("qte1").style.visibility = "hidden";
         document.getElementById("qte1").style.backgroundImage = ``;
-        key = 1;
+        health -= 1;
     }
 });
     setTimeout(() => {
         document.getElementById("qte1").style.visibility = "hidden";
         document.getElementById("qte1").style.backgroundImage = ``;
-        if (key == 1) {
-            
-        }
     }, 900);
 };
 
-function quick2() {
-    let key = 0;
+function quick2(a) {
     document.getElementById("qte2").style.transform = `translate(0px, 0px)`;
     document.getElementById("qte2").style.backgroundImage = `url(qte2.gif)`;
     const randomer2 = Math.floor(Math.random() * 700) + 100;
@@ -39,40 +36,32 @@ function quick2() {
     if (event.key === 'a') {
         document.getElementById("qte2").style.visibility = "hidden";
         document.getElementById("qte2").style.backgroundImage = ``;
-        key = 1;
+        health -= 1;
     }
 });
     setTimeout(() => {
         document.getElementById("qte2").style.visibility = "hidden";
         document.getElementById("qte2").style.backgroundImage = ``;
-        if (key == 1) {
-            
-        }
     }, 900);
 };
 
-function quick3() {
-    let key = 0;
+function quick3(p) {
     document.getElementById("qte3").style.transform = `translate(0px, 0px)`;
     document.getElementById("qte3").style.backgroundImage = `url(qte3.gif)`;
     const randomer3 = Math.floor(Math.random() * 700) + 100;
-    console.log(randomer3);
     const randomest3 = Math.floor(Math.random() * 550) + 25;
     document.getElementById("qte3").style.transform = `translate(${randomer3}px, ${randomest3}px)`;
     document.getElementById("qte3").style.visibility = `visible`;
     document.addEventListener('keyup', (event) => {
-    if (event.key === 'p') {
+    if (event.p === 'p') {
         document.getElementById("qte3").style.visibility = "hidden";
         document.getElementById("qte3").style.backgroundImage = ``;
-        key = 1;
+        health -= 1;
     }
 });
     setTimeout(() => {
         document.getElementById("qte3").style.visibility = "hidden";
         document.getElementById("qte3").style.backgroundImage = ``;
-        if (key == 1) {
-            
-        }
     }, 900);
 };
 
@@ -80,7 +69,6 @@ function quickattack() {
     one = quick1();
     two = quick2();
     three = quick3();
-    alert(one);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////
